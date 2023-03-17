55c3413db3d5d8aa6a18d2aaadc318de92b5bb9b
root <lmitless.claver@gmail.com>
2023-03-16 21:00:04 -0700

    advanced

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        write(2, &str[i], 1);
+    }
+    return (1);
+}
+
