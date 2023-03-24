bad828461cadb67ef978ada52e0ec2f3e6e21b7e
root <lmitless.claver@gmail.com>
2023-03-23 22:17:24 -0700

    bug fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,10 @@
 /**
- * main - Entry point
+ * main - entry point
  *
- * Description: Prints numbers from 1 to 100 with
- * Fizz for multiples of 3, Buzz for multiples of 5,
- * and FizzBuzz for multiples of both 3 and 5
+ * Description: Prints the numbers from 1 to 100, replacing multiples of three
+ *              with Fizz, multiples of five with Buzz, and multiples of both
+ *              with FizzBuzz.
+ *              Each number or word is separated by a space.
  *
  * Return: Always 0 (Success)
  */
@@ -17,17 +18,23 @@ int main(void)
         for (i = 1; i <= 100; i++)
         {
                 if (i % 3 == 0 && i % 5 == 0)
+                {
                         printf("FizzBuzz ");
+                }
                 else if (i % 3 == 0)
+                {
                         printf("Fizz ");
+                }
                 else if (i % 5 == 0)
+                {
                         printf("Buzz ");
+                }
                 else
+                {
                         printf("%d ", i);
+                }
         }
-
         printf("\n");
 
         return (0);
 }
-
