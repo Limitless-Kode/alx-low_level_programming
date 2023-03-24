5e180bd92e917922a5ecfa4b31a0589c0b7feb40
root <lmitless.claver@gmail.com>
2023-03-23 22:35:55 -0700

    bug fixes

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-
 /**
-* main - An infinite loop fixed by commenting it out.
-* Description: This program shows how to comment out an infinit loop
-*              to avoid infinte loop error. It aslo uses escape sequences
-*              to print a message with a ne w line character
-* Return: Always 0 (Success)
-*/
+ * main - causes an infinite loop
+ *
+ * Return: 0
+ */
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
-/**
-* infinite loop - commented out
-* while (i < 10)
-* {
-* putchar(i);
-* }
+
+/*
+*while (i < 10)
+*{
+*Infinite loop - No increase of variable
+*putchar(i);
+}
 */
-printf("Infinite loop avoided! \\o// \n");
+printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
