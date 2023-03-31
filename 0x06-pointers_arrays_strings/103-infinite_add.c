3c748b6f2faff3b0805e59d7f0294c1191a666db
root <lmitless.claver@gmail.com>
2023-03-30 20:04:43 -0700

    added 103

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,59 +1,78 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers.
- *
- * @n1: The first number.
- * @n2: The second number.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- *
- * Return: A pointer to the result, or 0 if the result cannot be stored in r.
+ * rev_string - reverse array
+ * @n: integer params
+ * Return: 0
  */
+
+void rev_string(char *n)
+{
+	int i = 0;
+	int j = 0;
+	char temp;
+
+	while (*(n + i) != '\0')
+	{
+		i++;
+	}
+	i--;
+
+	for (j = 0; j < i; j++, i--)
+	{
+		temp = *(n + j);
+		*(n + j) = *(n + i);
+		*(n + i) = temp;
+	}
+}
+
+/**
+ * infinite_add - add 2 numbers together
+ * @n1: text representation of 1st number to add
+ * @n2: text representation of 2nd number to add
+ * @r: pointer to buffer
+ * @size_r: buffer size
+ * Return: pointer to calling function
+ */
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-        int len1 = 0, len2 = 0, i = 0, j = 0, sum = 0, carry = 0;
+	int overflow = 0, i = 0, j = 0, digits = 0;
+	int val1 = 0, val2 = 0, temp_tot = 0;
 
-        /* Find lengths of n1 and n2 */
-        while (n1[len1] != '\0')
-                len1++;
-        while (n2[len2] != '\0')
-                len2++;
-
-        /* Check if the result can be stored in r */
-        if (len1 + 1 > size_r || len2 + 1 > size_r)
-                return (0);
-
-        /* Add digits from right to left */
-        for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0 || carry; i--, j--)
-        {
-                sum = carry;
-                if (i >= 0)
-                        sum += n1[i] - '0';
-                if (j >= 0)
-                        sum += n2[j] - '0';
-                if (sum > 9)
-                {
-                        carry = 1;
-                        sum -= 10;
-                }
-                else
-                {
-                        carry = 0;
-                }
-                r[len1 + len2 - (i + j + 2)] = sum + '0';
-        }
-
-        /* Add null terminator */
-        r[len1 + len2 - (i + j + 2)] = '\0';
-
-        /* Reverse result */
-        for (i = 0, j = len1 + len2 - 2; i < j; i++, j--)
-        {
-                char tmp = r[i];
-                r[i] = r[j];
-                r[j] = tmp;
-        }
-
-        return (r);
+	while (*(n1 + i) != '\0')
+		i++;
+	while (*(n2 + j) != '\0')
+		j++;
+	i--;
+	j--;
+	if (j >= size_r || i >= size_r)
+		return (0);
+	while (j >= 0 || i >= 0 || overflow == 1)
+	{
+		if (i < 0)
+			val1 = 0;
+		else
+			val1 = *(n1 + i) - '0';
+		if (j < 0)
+			val2 = 0;
+		else
+			val2 = *(n2 + j) - '0';
+		temp_tot = val1 + val2 + overflow;
+		if (temp_tot >= 10)
+			overflow = 1;
+		else
+			overflow = 0;
+		if (digits >= (size_r - 1))
+			return (0);
+		*(r + digits) = (temp_tot % 10) + '0';
+		digits++;
+		j--;
+		i--;
+	}
+	if (digits == size_r)
+		return (0);
+	*(r + digits) = '\0';
+	rev_string(r);
+	return (r);
 }
