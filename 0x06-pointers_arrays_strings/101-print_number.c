92d9c06ca8b0037319a9f2317a494b7f02f2e158
root <lmitless.claver@gmail.com>
2023-03-30 20:06:52 -0700

    bug fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,23 +1,26 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
-  * print_number - prints an integer
-  * @n: integer value
-  *
-  * Return: void
-  */
+ * print_number - print numbers chars
+ * @n: integer params
+ * Return: 0
+ */
 
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10 != 0)
+	{
+		print_number(n1 / 10);
+	}
+	_putchar((n1 % 10) + '0');
 }
-
-if (n / 10)
-print_number(n / 10);
-
-_putchar((n % 10) + '0');
-}
-
