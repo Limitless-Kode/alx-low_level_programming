abd1931e54aafada3b893418165b949be45472ea
root <lmitless.claver@gmail.com>
2023-03-30 20:11:48 -0700

    bug fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,78 +1,59 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
- * Return: 0
+ * infinite_add - Adds two numbers.
+ *
+ * @n1: First number.
+ * @n2: Second number.
+ * @r: Buffer to store the result.
+ * @size_r: Size of the buffer.
+ *
+ * Return: Pointer to the result.
  */
-
-void rev_string(char *n)
-{
-	int i = 0;
-	int j = 0;
-	char temp;
-
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
-
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
-}
-
-/**
- * infinite_add - add 2 numbers together
- * @n1: text representation of 1st number to add
- * @n2: text representation of 2nd number to add
- * @r: pointer to buffer
- * @size_r: buffer size
- * Return: pointer to calling function
- */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
-	int val1 = 0, val2 = 0, temp_tot = 0;
+    int len1, len2, len, carry, i, j, k;
 
-	while (*(n1 + i) != '\0')
-		i++;
-	while (*(n2 + j) != '\0')
-		j++;
-	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
-		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
-	{
-		if (i < 0)
-			val1 = 0;
-		else
-			val1 = *(n1 + i) - '0';
-		if (j < 0)
-			val2 = 0;
-		else
-			val2 = *(n2 + j) - '0';
-		temp_tot = val1 + val2 + overflow;
-		if (temp_tot >= 10)
-			overflow = 1;
-		else
-			overflow = 0;
-		if (digits >= (size_r - 1))
-			return (0);
-		*(r + digits) = (temp_tot % 10) + '0';
-		digits++;
-		j--;
-		i--;
-	}
-	if (digits == size_r)
-		return (0);
-	*(r + digits) = '\0';
-	rev_string(r);
-	return (r);
+    /* Get length of n1 */
+    for (len1 = 0; n1[len1] != '\0'; len1++)
+        ;
+    /* Get length of n2 */
+    for (len2 = 0; n2[len2] != '\0'; len2++)
+        ;
+
+    /* Determine length of result */
+    len = len1 > len2 ? len1 + 1 : len2 + 1;
+
+    /* Check if result fits in buffer */
+    if (len > size_r)
+        return (0);
+
+    /* Add numbers digit by digit */
+    for (i = len1 - 1, j = len2 - 1, k = len - 1, carry = 0; k >= 0; i--, j--, k--)
+    {
+        int digit1 = i >= 0 ? n1[i] - '0' : 0;
+        int digit2 = j >= 0 ? n2[j] - '0' : 0;
+
+        int sum = digit1 + digit2 + carry;
+
+        r[k] = sum % 10 + '0';
+        carry = sum / 10;
+    }
+
+    /* Remove leading zeros */
+    for (i = 0; r[i] == '0' && i < len - 1; i++)
+        ;
+
+    /* Shift result left if necessary */
+    if (i > 0)
+    {
+        len -= i;
+        for (j = 0; j < len; j++)
+            r[j] = r[j + i];
+    }
+
+    /* Add null terminator */
+    r[len] = '\0';
+
+    return (r);
 }
